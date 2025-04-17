4a09efa2e70bb3dd55f4497db144ab06ef8eb216
jujulinho-ui <jucsts@gmail.com>
2025-04-17 17:12:51 -0300

    Update agent-toolkit1.js

--- agent-toolkit1.js
+++ agent-toolkit1.js
@@ -1,19 +1,16 @@
-// agent‑toolkit.js
+// agent-toolkit1.js
 (function() {
   function init() {
     alert('🚀 Toolkit demo carregado com sucesso!');
   }
 
   function waitForToolkit() {
-    // Quando a API do Toolkit estiver disponível, chama o init()
     if (window.Toolkit && typeof Toolkit.getToolbar === 'function') {
       init();
     } else {
-      // Se não, tenta de novo em 200ms
       setTimeout(waitForToolkit, 200);
     }
   }
 
-  // Inicia o loop de espera
   waitForToolkit();
 })();
