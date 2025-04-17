942010d5aa1a805aaec34de7d8275a53f541b21a
jujulinho-ui <jucsts@gmail.com>
2025-04-17 18:29:07 -0300

    Update agent-toolkit1.js

--- agent-toolkit1.js
+++ agent-toolkit1.js
@@ -1,15 +1,1 @@
-console.log('🔥 agent-toolkit1.js foi injetado');
-(function() {
-  function init() {
-    console.log('✅ Toolkit está disponível');
-    alert('🚀 Toolkit demo carregado com sucesso!');
-  }
-  function waitForToolkit() {
-    if (window.Toolkit && typeof Toolkit.getToolbar === 'function') {
-      init();
-    } else {
-      setTimeout(waitForToolkit, 200);
-    }
-  }
-  waitForToolkit();
-})();
+   alert('🚨 Teste: meu JS está rodando!');
